e8331e50b53b9771c5391e6c24f4a7a4fbf0306a
Joachim Haagen Skeie <joachim@skeiene.no>
2026-02-05 18:51:01 +0100

    Legger til en egen app.css fil

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+    import '../app.css';
     import Header from "$lib/components/Header.svelte";
 </script>
 
@@ -7,16 +8,5 @@
 <slot></slot>
 
 <style>
-    :global(a) {
-        text-decoration: none;
-        color: inherit; /* Optional: to keep the link color consistent with surrounding text */
-    }
 
-    :global(a:hover) {
-        color: #007bff; /* Optional: change color on hover */
-    }
-
-    :global(a:visited) {
-        color: inherit; /* Optional: change color for visited links */
-    }
 </style>
